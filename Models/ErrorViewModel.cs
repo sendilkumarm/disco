f371d0a80de46a7f05e014bd22b4266c0f5fd7bd
sendilkumar <semu4099@gmail.com>
2018-12-02 06:54:37 +0530

    Newly Created

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace disco.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
